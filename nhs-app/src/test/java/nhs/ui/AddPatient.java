705a63e0b678087db21027c5211156315c953af3
priyanka-racha <priyanka.racha167@gmail.com>
2019-06-14 23:46:16 +0100

    Updated script for login and add patient

--- nhs-app/src/test/java/nhs/ui/AddPatient.java
+++ nhs-app/src/test/java/nhs/ui/AddPatient.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 import org.junit.After;
@@ -15,6 +17,8 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import com.google.common.io.Files;
 
@@ -23,43 +27,62 @@ public class AddPatient {
 	static WebDriver driver;
 
 	@Before
-	public void startBrowser() {
-		// for initializations or keeping the system ready
-		System.setProperty("webdriver.chrome.driver", "P:\\Selenium\\chromedriver.exe");
-		driver = new ChromeDriver();
+	public void startBrowser() throws MalformedURLException {
+		// for initializations or keeping the system ready-Testing application on local machine
+		//System.setProperty("webdriver.chrome.driver", "P:\\Selenium\\chromedriver.exe");
+		//driver = new ChromeDriver();
+		//Testing application on remote server
+		DesiredCapabilities capability = DesiredCapabilities.chrome();
+		driver = new RemoteWebDriver(new URL("http://192.168.99.100:4444/wd/hub"), capability);
 		driver.get("http://hms-qa.herokuapp.com/");
 		driver.manage().window().maximize();
 	}
 
-	@Test
-	public void testAddPatient() throws IOException, InterruptedException {
+	public void testAddPatient(String firstName, String LastName, String hosiptal_Number, String dateofbirth,
+			String sex) throws IOException, InterruptedException {
 		// for initializations or keeping the system ready
 		String email = "tester3";
 		String pwd = "tester3";
 		driver.findElement(By.id("inputEmail")).sendKeys(email);
 		driver.findElement(By.id("inputPassword")).sendKeys(pwd);
+		//Clicking Sign-in button
 		WebElement element = driver.findElement(By.xpath("//*[@class='btn btn-lg btn-primary btn-block btn-signin']"));
 		element.click();
+		Thread.sleep(1000);
+		//Filling first_name,Last_name,hosiptal_number,date_of_birth,sex 
 		driver.findElement(By.xpath("//*[@id=\"side-menu\"]/li[2]/a")).click();
-		driver.findElement(By.xpath("//*[@id=\"first-name\"]")).sendKeys("abcd");
-		driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/form/div[2]/div/input")).sendKeys("1234");
-		driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/form/div[3]/div/input")).sendKeys("HMS234");
+		driver.findElement(By.xpath("//*[@id=\"first-name\"]")).sendKeys(firstName);
+		driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/form/div[2]/div/input")).sendKeys(LastName);
+		driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/form/div[3]/div/input"))
+				.sendKeys(hosiptal_Number);
 		driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/form/div[4]/div/input"))
-				.sendKeys("05/11/2014");
-		driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/form/div[5]/div/div/label[1]")).click();
+				.sendKeys(dateofbirth);
+		if (sex == "male") {
+			WebElement male = driver
+					.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/form/div[5]/div/div/label[1]"));
+			male.click();
+		} else {
+			WebElement female = driver
+					.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/form/div[5]/div/div/label[2]"));
+			female.click();
+		}
+		// Selecting diseases in Add patient table
 		driver.findElement(By.xpath("//*[@id=\"add-new-patient\"]/tbody/tr[3]/td[3]/input")).click();
 		driver.findElement(By.xpath("//*[@id=\"add-new-patient\"]/tbody/tr[4]/td[3]/input")).click();
+		Thread.sleep(2000);
+		// clicking Add patient button
 		driver.findElement(By.xpath("//*[@id=\"page-wrapper\"]/div/div[2]/form/input")).click();
+		// Going back to dashboard
 		driver.findElement(By.xpath("//*[@id=\"side-menu\"]/li[1]/a")).click();
 		Thread.sleep(3000);
-		File src1 = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		//File src1 = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 		// Now copy the screenshot to the desired location using copyfile method
-		Files.copy(src1, new File("P:\\togetherleap\\Screenshots_NHS\\Table.jpg"));
-		patientWaiting();
+		//Files.copy(src1, new File("P:\\togetherleap\\Screenshots_NHS\\Table.jpg"));
+		findPatientWaiting(firstName);
 
 	}
 
-	public void patientWaiting() {
+	public void findPatientWaiting(String firstName) {
 		WebElement waitingTable = driver.findElement(By.xpath("//*[@id=\"patients-waiting\"]"));
 		// Get all rows
 		List<WebElement> rows = waitingTable.findElements(By.tagName("tr"));
@@ -70,7 +93,7 @@ public class AddPatient {
 			List<WebElement> cols = row.findElements(By.tagName("td"));
 			for (WebElement col : cols) {
 				System.out.print(col.getText() + "\t");
-				if (col.getText().contains("Abcd")) {
+				if (col.getText().contains(firstName)) {
 					assertTrue(true);
 					System.out.println("Patient found");
 					return;
@@ -80,6 +103,15 @@ public class AddPatient {
 		}
 		System.out.println("Patient not found");
 		assertTrue(false);
+	}
+
+	@Test
+	public void testAddPatient1() throws IOException, InterruptedException {
+		testAddPatient("Lussi", "Kite", "HMS2345", "09/2/2016", "female");
+		Thread.sleep(3000);
+		//File src2 = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		//Files.copy(src2, new File("P:\\togetherleap\\Screenshots_NHS\\Table2.jpg"));
+
 	}
 
 	@After
--- nhs-app/src/test/java/nhs/ui/NHS_Login.java
+++ nhs-app/src/test/java/nhs/ui/NHS_Login.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+//import java.net.URL;
 import java.util.List;
 
 import org.junit.After;
@@ -15,6 +18,10 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.remote.DesiredCapabilities;
+//import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import com.google.common.io.Files;
 
@@ -23,11 +30,14 @@ public class NHS_Login {
 	// Global variable
 	static WebDriver driver;
 
-	 @Before
-	public void startBrowser() {
-			// for initializations or keeping the system ready
-		System.setProperty("webdriver.chrome.driver", "P:\\Selenium\\chromedriver.exe");
-		driver = new ChromeDriver();
+	@Before
+	public void startBrowser() throws MalformedURLException {
+		// for initializations or keeping the system ready
+		// System.setProperty("webdriver.chrome.driver",
+		// "P:\\Selenium\\chromedriver.exe");
+		// driver = new ChromeDriver();
+		DesiredCapabilities capability = DesiredCapabilities.chrome();
+		driver = new RemoteWebDriver(new URL("http://192.168.99.100:4444/wd/hub"), capability);
 	}
 
 	// Testcase:1- Verifying sign-in page
@@ -41,54 +51,37 @@ public class NHS_Login {
 		driver.findElement(By.id("inputEmail")).sendKeys(email);
 		driver.findElement(By.id("inputPassword")).sendKeys(pwd);
 		// Take screenshot and store as file format
-		File src1 = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-		// Now copy the screenshot to the desired location using copyfile method
-		Files.copy(src1, new File("P:\\togetherleap\\Screenshots_NHS\\Capture1.jpg"));
+		// File src1 = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		// Now copy the screenshot to the desired location using copy file method
+		// Files.copy(src1, new File("~/LoginScreen.jpg"));
 		// Click on the "SUBMIT button" for NHS login page
 		WebElement element = driver.findElement(By.xpath("//*[@class='btn btn-lg btn-primary btn-block btn-signin']"));
 		element.click();
 		Thread.sleep(5000);
-		File src2 = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-		Files.copy(src2, new File("P:\\togetherleap\\Screenshots_NHS\\Capture2.jpg"));
-		// Stopping the screen for 5 seconds
-		Thread.sleep(2000);
-		// Kill the browser
-		System.out.println("Login Successful");
-
+		// File src2 = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		// Files.copy(src2, new File("~/HomePage.jpg"));
+		// Stopping the screen in 1 second
+		Thread.sleep(1000);
 	}
 
-	
 	@Test
-	public void testLogin() throws InterruptedException, IOException {
-		NHS_Login obj = new NHS_Login();
+	public void testLogin1() throws InterruptedException, IOException {
+		// NHS_Login obj = new NHS_Login();
 		// Test case:2 - Verify with registered username & password
-		obj.login("tester3", "tester3");
+		login("tester3", "tester3");
 		List<WebElement> elements = driver.findElements(By.id("inputEmail"));
-		//System.out.println("elements: " + elements.size());
+		// System.out.println("elements: " + elements.size());
+		// Both expected and actual values are same then zero ,failed-1
 		assertEquals(0, elements.size());
-
-		// Test case:3 - Verify with registered username & invalid password
-		//obj.login("tester3", "test@#$");
-		 //elements = driver.findElements(By.id("inputEmail"));
-		//assertEquals(1, elements.size());
-
-		// test case:4 - Verify with invalid username & invalid password (Unregistered)
-		// obj.login("sdfg&", "34rf6y");
-
 	}
-	
-	
+
 	@Test
 	public void testLogin2() throws InterruptedException, IOException {
 		NHS_Login obj1 = new NHS_Login();
 		// Test case:3 - Verify with registered username & invalid password
 		obj1.login("tester3", "test@#$");
 		List<WebElement> elements = driver.findElements(By.id("inputEmail"));
-		assertEquals(0, elements.size());
-
-		// test case:4 - Verify with invalid username & invalid password (Unregistered)
-		// obj.login("sdfg&", "34rf6y");
-
+		assertEquals(1, elements.size());
 	}
 
 	@After
